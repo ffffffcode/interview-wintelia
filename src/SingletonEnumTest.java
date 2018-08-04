a61e929641d7ec77a7b79362838c494c9dddc119
AaronFae <AaronFae@163.com>
2018-08-04 17:17:05 +0800

    add Test of SingletonEnum

---
+++ src/SingletonEnumTest.java
@@ -0,0 +1,21 @@
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+public class SingletonEnumTest {
+	public static void main(String[] args) {
+		Runnable target = new Runnable() {
+			@Override
+			public void run() {
+				System.out.println(SingletonEnum.INSTANCE.hashCode());
+			}
+		};
+
+		ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(500, 1000, 2, TimeUnit.SECONDS,
+				new ArrayBlockingQueue<Runnable>(1000));
+		for (int i = 0; i < 1000; i++) {
+			threadPoolExecutor.execute(target);
+		}
+		threadPoolExecutor.shutdown();
+	}
+}
